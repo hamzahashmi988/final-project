0ec9a3d4ccce5d2ff57acba6bbc47a8366b546fa
MUHAMMAD HAMZA HASHMI <110339983+hamzahashmi988@users.noreply.github.com>
2024-12-22 19:47:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for form submission
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
